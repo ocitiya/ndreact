6b6938b1f8090c1219906617f273738beb4fd165
Muhammad Rasyidi <muhammadrasyidi17@gmail.com>
2020-03-23 08:43:25 +0700

    add .env

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 const path = require('path');
 
-const port = 5000;
+const port = process.env.SERVER_PORT;
 
 app.use(express.static('build'));
 
